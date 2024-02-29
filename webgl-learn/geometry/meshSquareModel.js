3ccdc9572aff79b146da866d337e9e292c56e19d
aliveAmy <aliveAmy719@gmail.com>
2024-02-29 10:39:56 +0800

    feat: vertices indexs

--- webgl-learn/geometry/meshSquareModel.js
+++ webgl-learn/geometry/meshSquareModel.js
@@ -1,22 +1,36 @@
 import * as THREE from 'three';
 
-const vertics = new Float32Array([
+// const vertics = new Float32Array([
+//     0,0,0, // 顶点1坐标
+//     80,0,0, // 顶点2坐标
+//     80,80,0, // 顶点3坐标
+//     0,0,0, // 顶点4坐标
+//     80,80,0, // 顶点5坐标
+//     0,80,0// 顶点6坐标
+// ]);
+
+const verticsCommonIndex = new Float32Array([
     0,0,0, // 顶点1坐标
     80,0,0, // 顶点2坐标
     80,80,0, // 顶点3坐标
-    0,0,0, // 顶点4坐标
-    80,80,0, // 顶点5坐标
-    0,80,0// 顶点6坐标
-]);
+    0,80,0// 顶点4坐标
+])
 
 // 顶点4、5、6 只要和顶点1、2、3保持一个方向就可以了，同为顺时针或同为逆时针。顺序对结果没有产生影响
 
 
 const geometry = new THREE.BufferGeometry();
 
-const attribute  = new THREE.BufferAttribute(vertics, 3);
+// const attribute  = new THREE.BufferAttribute(vertics, 3);
+const attribute  = new THREE.BufferAttribute(verticsCommonIndex, 3);
 
 geometry.attributes.position = attribute;
+
+const indexes = new Uint16Array([0,1,2,2,3,0]);
+
+const indexAttrbute = new THREE.BufferAttribute(indexes, 1);
+
+geometry.index = indexAttrbute;
 
 const material = new THREE.MeshBasicMaterial({
     color: 0X00ffff,
